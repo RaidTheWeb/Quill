607ed47dbd7c1a1dd5edd2447e929a2718c8217f
sugarfi <jacksongang3000@gmail.com>
2020-04-04 21:44:00 -0400

    comment runner.py

--- src/runner.py
+++ src/runner.py
@@ -25,117 +25,127 @@ def ref(obj):
 
 def call(obj, *args):
     # Attempts to call an object.
-	obj = ref(obj) # Make sure we aren't calling a reference.
-	if get(obj, 'call', error=False):
+	obj = ref(obj) # Make sure we aren't calling a reference
+	if get(obj, 'call', error=False): # Check for call method
 		func = get(obj, 'call')
-		if isinstance(obj, data.Method):
+		if isinstance(obj, data.Method): # If we find it, call that
 			return func(*args)
 		return get(obj, 'call').attrs['_call'](*args)
-	elif get(obj, '_call', error=False):
+	elif get(obj, '_call', error=False): # Check for _call method
 		func = get(obj, '_call')
-		if isinstance(obj, data.Method):
+		if isinstance(obj, data.Method): # If we find it, call that
 			return func(*args)
 		return get(obj, '_call').attrs['_call'](*args)
 	else:
-		errors.error(f'Object {obj.string().val} is not callable')
+		errors.error(f'Object {obj.string().val} is not callable') # Otherwise, the object is not callable
 
 
 def get(obj, attr, error=True):
-	obj = ref(obj)
-	attr = data.Symbol(attr) # select it and use ctrl-[ and ctrl-]
-	if 'get' in obj.attrs:
+    # Gets an attribute of an object.
+	obj = ref(obj) # Make sure we don't have a reference
+	attr = data.Symbol(attr) # Convert the attribute to a symbol
+	if 'get' in obj.attrs: # If the object has a get method, call that
 		return call(obj.attrs['get'], attr)
-	elif '_get' in obj.attrs:
+	elif '_get' in obj.attrs: # If it has a _get method, call that
 		return call(obj.attrs['_get'], attr)
-	elif attr.val in obj.attrs:
+	elif attr.val in obj.attrs: # Otherwise, check in the object attributes
 		return obj.attrs[attr.val]
 	else:
 		if error:
-			errors.error(f'Cannot get attribute of object {obj.string().val}')
+			errors.error(f'Cannot get attribute of object {obj.string().val}') # If error is True, throw an error
 		else:
-			return
+			return # Otherwise, return None
 
 def op(obj, op):
-	obj = ref(obj)
-	if op.val not in obj.attrs:
-		if op.val in op_names:
-			for name in op_names[op.val]:
-				if get(obj, name, error=False):
+    # Gets the method for an operator of an object.
+	obj = ref(obj) # Again, make sure we don't have a reference
+	if op.val not in obj.attrs: # Only go through op_names if the object doesn't already have the operator as an attribute
+		if op.val in op_names: # Check if it's in op_names
+			for name in op_names[op.val]: # Iterate over every name, usually just one
+				if get(obj, name, error=False): # Check for the name
 					return get(obj, data.Symbol(name))
-				elif get(obj, '_' + name, error=False):
+				elif get(obj, '_' + name, error=False): # Check for the name prefixed with an underscore
 					return get(obj, data.Symbol('_' + name))
-				else:
+				else: # If it doesn't have either of those, throw an error
 					errors.error(f'{obj.string().val} does not have operator {op.string().val}')
 		else:
-			return get(obj, data.Symbol(op))
+			return get(obj, data.Symbol(op)) # Throw an error otherwise
+    return obj.attrs[op.val]
 
 def get_name(name, scope):
-	name = name.split('.')
-	while name:
-		part = name.pop(0)
-		old = scope
-		scope = get(scope, data.Symbol(part))
+    # Given a scope, splits a name into parts seperated by dots.
+	name = name.split('.') # Split the name
+	while name: # Iterate over every part of the name
+		part = name.pop(0) # Get one part of the name
+		old = scope # Store the old scope
+		scope = get(scope, data.Symbol(part)) # Get the next scope
 		if not scope:
-			errors.error(f'Object {old.string().val} has no attribute {part}')
+			errors.error(f'Object {old.string().val} has no attribute {part}') # Throw an error if the attribute doesn't exist
 	return scope
 
 def expr(val, scope):
-	if val.type == 'string':
+    # Evaluates one node in the AST.
+	if val.type == 'string': # If it is a string, return that
 	    return data.String(val.val[0])
-	elif val.type == 'number':
+	elif val.type == 'number': # If it is a number, return that
 	    return data.Number(val.val[0])
-	elif val.type == 'decl':
-		t = call(get_name(val.val[0], scope))
+	elif val.type == 'decl': # If it is a declaration, declare it
+		t = call(get_name(val.val[0], scope)) # Get the type, and call it, given us the type object
 		if t == data.Number:
-			default = data.Number(0)
+			default = data.Number(0) # The default value for a number is 0
 		elif t == data.String:
-			default = data.String('')
+			default = data.String('') # The default value for a string is the empty string
 		elif t == data.Func:
+            # The default value of a function is an empty AST
 			default = data.Func(scope, data.Block(Program(parse.Node('program'))))
 			default.val.val.globals = scope
 		elif t == data.Class:
+            # The default value for a class is the same as for functions
 			default = data.Class(scope, data.Block(Program(parse.Node('program'))))
 			default.val.val.globals = scope
 		elif t == data.List:
+            # The default value for a list is empty
 			default = data.List(data.Type)
 		else:
+            # Otherwise, just create a type object
 			default = data.Type()
-		scope.set(data.Symbol(val.val[1]), default)
-	elif val.type == 'name':
+		scope.set(data.Symbol(val.val[1]), default) # Set the name to the default value
+	elif val.type == 'name': # If it is a name, simply get the value of the name
 		return get_name(val.val[0], scope)
-	elif val.type == 'call':
-		func = expr(val.val[0], scope)
+	elif val.type == 'call': # If it is a function call, evaluate that
+		func = expr(val.val[0], scope) # Get the function
 		args = []
-		for arg in val.val[1].val:
-			if arg.type == 'decl':
+		for arg in val.val[1].val: # Create the list of arguments
+			if arg.type == 'decl': # Don't evaluate arguments that are declarations
 				args.append(arg)
-			else:
+			else: # Evaluate all other arguments
 				args.append(expr(arg, scope))
-		return call(func, *args)
-	elif val.type == 'op':
-		a = expr(val.val[0], scope)
-		func = op(a, data.Symbol(val.val[1]))
-		return call(func, expr(val.val[2], scope))
-	elif val.type == 'block':
-		program = Program(val.val[0]);
-		program.globals = scope
-		return data.Block(program)
-	elif val.type == 'array':
+		return call(func, *args) # Call the function
+	elif val.type == 'op': # If it is an operator, we need to call op
+		a = expr(val.val[0], scope) # Get the left-hand side
+		func = op(a, data.Symbol(val.val[1])) # Get the method to call
+		return call(func, expr(val.val[2], scope)) # Call the left-hand side with the right one
+	elif val.type == 'block': # If it is a block, create a new Program
+		program = Program(val.val[0]) # Create the object
+		program.globals = scope # Set the scope
+		return data.Block(program) # Return a block
+	elif val.type == 'array': # If it  is a list literal, build a list
 		array = []
 		for item in val.val[0].val:
-			array.append(expr(item, scope))
-		return data.List(type(array[0]), *array)
-	elif val.type == 'index':
+			array.append(expr(item, scope)) # Evaluate each item in turn
+		return data.List(type(array[0]), *array) # Create an array
+	elif val.type == 'index': # If it is an indexing expression, simply call the index operator
 		index = op(expr(val.val[0], scope), data.Symbol('index'))
 		return call(index, expr(val.val[1], scope))
-	elif val.type == 'child':
+	elif val.type == 'child': # If it is a child expression (a.b), simply call the get method of the parent
 		return get(expr(val.val[0], scope), val.val[1])
 
 class Program():
+    # The actual program object. Stores the AST and the global scope.
 	def __init__(self, ast):
 		self.ast = ast
-		self.globals = data.Map(data.Symbol, data.Type)
-		self.globals.set(data.Symbol('import'), data.Method(self._import))
+		self.globals = data.Map(data.Symbol, data.Type) # Set up globals to map symbols to anything
+		self.globals.set(data.Symbol('import'), data.Method(self._import)) # Set up all the builtins
 		self.globals.set(data.Symbol('if'), data.Method(self._if))
 		self.globals.set(data.Symbol('return'), data.Method(lambda *args: args[0] if args else None))
 		self.globals.set(data.Symbol('py'), data.Method(lambda x: eval(x.val)))
@@ -150,28 +160,34 @@ class Program():
 		self.globals.set(data.Symbol('map'), type_method(data.Map))
 		self.globals.set(data.Symbol('void'), data.Method(lambda: type(None)))
 
-	def print(self, *args): #recursion moment <----- recursion is its own reward
-		for val in args: # also i found the problem
-			print(call(get(val, '_string')).val) # i'm gonna fix it
+	def print(self, *args):
+        # Prints every value passed to it as a string.
+		for val in args:
+			print(call(get(val, '_string')).val)
 	def _import(self, *args):
+        # Loads a .qyl file relative to the current directory and adds it's scope to the global one.
 		name = args[0].val
 		try:
-			file = open(f'{name}.qyl')
-			ast = parse.Parser().parse(parse.Lexer().tokenize(file.read()))
-			program = Program(ast)
-			program.run()
+			file = open(f'{name}.qyl') # Open the file
+			ast = parse.Parser().parse(parse.Lexer().tokenize(file.read())) # Lex and parse it
+			program = Program(ast) # Create a program object
+			program.run() # Run it
+             # Set the name of the library in globals to a map containing the exported variables from that module.
 			self.globals.set(data.Symbol(name.split('/')[-1]), program.globals)
-		except FileNotFoundError:
+		except FileNotFoundError: # Throw an error if the file is not found.
 			errors.error('File not found')
 	def _if(self, *args):
-		args[0].val.globals = self.globals
-		if data.Bool(args[1]).val:
-			call(args[0])
+        # Executes the block passed to it only if the expression given to it is true.
+		args[0].val.globals = self.globals # Set the scope of the block to run.
+		if data.Bool(args[1]).val: # Convert the value to a boolean.
+			call(args[0]) # Call the block if it is true.
 	def run(self):
-		for node in self.ast.val[:-1]:
+        # Runs the AST.
+		for node in self.ast.val[:-1]: # Run all but the last node as expressions.
 			expr(node, self.globals)
-		return expr(self.ast.val[-1], self.globals)
+		return expr(self.ast.val[-1], self.globals) # Return the value of the last node.
 
 
 def run(ast):
+    # Shorthand for Program(ast).run()
     Program(ast).run()
