e2b77cf43b6fc132d512419c802ac448b0530b31
Solomon Jackson <jacksongang3000@gmail.com>
2020-04-09 01:05:02 +0000

    add repl

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ sys.path.append('./src')
 
 import parse
 import runner
+import errors
 
 l = parse.Lexer()
 p = parse.Parser()
 
-code = open(sys.argv[1]).read()
+if sys.argv[1:]:
+	code = open(sys.argv[1]).read()
 
-tree = p.parse(l.tokenize(code))
-runner.run(tree)
+	tree = p.parse(l.tokenize(code))
+	runner.run(tree)
+else:
+	errors.setno()
+	program = runner.Program(parse.Node('program'))
+	while True:
+		code = input('> ')
+		tree = p.parse(l.tokenize(code))
+		program.ast = tree
+		val = program.run()
+		if val:
+			program.print(val)
